bcd9244c6aa9c6270158e96f82e49dbddf5ffc75
muhaimin <meenkun4649@gmail.com>
2023-08-03 18:14:22 +0800

    ui: minor adjustment

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <li class="flex flex-col col-span-1 text-center bg-white divide-y divide-gray-200 rounded-lg shadow hover:cursor-pointer hover:shadow-lg">
+  <li class="flex flex-col col-span-1 text-center duration-300 bg-white divide-y divide-gray-200 rounded-lg shadow hover:cursor-pointer sm:hover:shadow-lg sm:hover:scale-105">
     <div class="flex flex-col flex-1 p-4 rounded-lg card-color">
-      <div class="flex items-center justify-center mx-auto rounded-full h-36 w-36 bg-sky-200">
-        <img class="w-24 h-24" :src="getImageUrl" alt="" />
+      <div class="flex items-center justify-center mx-auto bg-white rounded-full h-36 w-36">
+        <img :src="getImageUrl" alt="" />
       </div>
       <h3 class="mt-3 text-sm font-medium text-sky-500">#{{number}}</h3>
       <h3 class="text-xl font-semibold text-sky-900">{{name}}</h3>
@@ -32,6 +32,6 @@ export default {
 
 <style scoped>
 .card-color {
-  background: linear-gradient(150deg, #7dd3fc 50%, transparent 50%);
+  background: linear-gradient(150deg, #7dd3fc 55%, transparent 55%);
 }
 </style>
